786ecd2e891f0abc8b64b2a0a42eb4eebe2b616d
Aditya Nath Kalla <anathkalla@discoveryparks.com.au>
2023-07-31 09:22:13 +0930

    Update elevated_button color code

---
+++ lib/theme/dark_theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+import '../global/global.dart';
+
+ThemeData darkTheme = ThemeData(
+  brightness: Brightness.dark,
+  useMaterial3: true,
+  colorSchemeSeed: Colorof.primaryColor,
+);
